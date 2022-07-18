ab278a1128f2ce4a831e8d2e1683ba63c94b703c
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-18 18:35:06 -0300

    feat(controller.asset): Create controller
    The main responsibility of this controller to respond with an asset or
    an error message containing `Ativo não encontrado`

---
+++ backend/src/controller/controller.asset.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import assetService from '../service/service.asset';
+import HttpException from '../shared/http.exception';
+
+async function getByAssetCode(request: Request, response: Response): Promise<void> {
+  const { codAtivo } = request.params;
+
+  const asset = await assetService.getByAssetCode(Number(codAtivo));
+
+  if (!asset) throw new HttpException('Ativo não encontrado', StatusCodes.NOT_FOUND);
+
+  response.status(StatusCodes.OK).json(asset);
+}
+
+export default {
+  getByAssetCode,
+};
