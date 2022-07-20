f207235a359fe0036488b50cb8bf8e2d55d1628f
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-20 19:39:55 -0300

    dev(model.investment): Add new functions
    Create function to update a purchase
    
    Creates function that checks if the client already has the asset

--- backend/src/model/model.investment.ts
+++ backend/src/model/model.investment.ts
@@ -3,12 +3,41 @@ import { AssetPurchaseOrder } from '../interface';
 
 const prisma = new PrismaClient();
 
-function buyAsset(asset: AssetPurchaseOrder) {
+function buyNewAsset(asset: AssetPurchaseOrder) {
   return prisma.ativo_Cliente.create({
     data: asset,
   });
 }
 
+function verifyAssetAlreadyPurchased(CodCliente: number, CodAtivo: number) {
+  return prisma.ativo_Cliente.findUnique({
+    where: {
+      CodCliente_CodAtivo: {
+        CodCliente,
+        CodAtivo,
+      },
+    },
+  });
+}
+
+function updatePurchase(order: AssetPurchaseOrder) {
+  const { CodAtivo, CodCliente, QtdeAtivo } = order;
+
+  return prisma.ativo_Cliente.update({
+    where: {
+      CodCliente_CodAtivo: {
+        CodCliente,
+        CodAtivo,
+      },
+    },
+    data: {
+      QtdeAtivo,
+    },
+  });
+}
+
 export default {
-  buyAsset,
+  buyNewAsset,
+  verifyAssetAlreadyPurchased,
+  updatePurchase,
 };
