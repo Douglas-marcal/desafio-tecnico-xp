10a2b675a9cec7aff51ade1631733c50c2c41fb1
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-20 14:14:53 -0300

    feat(service.asset): Create service to register an asset

--- backend/src/service/service.asset.ts
+++ backend/src/service/service.asset.ts
@@ -1,9 +1,32 @@
+import { StatusCodes } from 'http-status-codes';
+import { Asset } from '../interface';
 import assetModel from '../model/model.asset';
+import HttpException from '../shared/http.exception';
 
 function getByAssetCode(assetCode: number) {
   return assetModel.getByAssetCode(assetCode);
 }
 
+async function registerAsset(asset: Asset) {
+  const alreadyExist = await assetModel.getByAssetName(asset.NomeAtivo);
+
+  const message = 'Não foi possível criar, já existe um ativo com este nome.';
+
+  if (alreadyExist) {
+    throw new HttpException(message, StatusCodes.CONFLICT);
+  }
+
+  const registeredAsset = await assetModel.registerAsset(asset);
+
+  const response = {
+    ...registeredAsset,
+    Valor: Number(registeredAsset.Valor),
+  };
+
+  return response;
+}
+
 export default {
   getByAssetCode,
+  registerAsset,
 };
