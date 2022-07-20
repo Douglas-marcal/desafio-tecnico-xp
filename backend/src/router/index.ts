218cb8542fabe84371c29191982dc46acffe55c6
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-20 15:03:34 -0300

    feat(router): Create router `/investimentos`

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import assetRouter from './asset';
 import clientRouter from './client';
+import investmentRouter from './investment';
 
 const router = Router();
 
 router.use('/ativos', assetRouter);
 
 router.use('/conta', clientRouter);
+
+router.use('/investimentos', investmentRouter);
 
 export default router;
---
+++ backend/src/router/investment.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/comprar');
+
+router.post('/vender');
+
+export default router;
