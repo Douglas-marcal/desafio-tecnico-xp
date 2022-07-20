955d3141ecf0e5889f4d37800fcfa33ffa7eb328
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-20 14:27:17 -0300

    feat(router): Create router to list all assets

--- backend/src/router/asset.ts
+++ backend/src/router/asset.ts
@@ -3,6 +3,8 @@ import assetController from '../controller/controller.asset';
 
 const router = Router();
 
+router.get('/', assetController.getAllAssets);
+
 router.post('/registrar', assetController.registerAsset);
 
 router.get('/:codAtivo', assetController.getByAssetCode);
