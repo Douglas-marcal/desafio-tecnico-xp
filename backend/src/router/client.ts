8b3d4521c8e00c745cb837c1b565590268fa4519
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-21 17:41:39 -0300

    refactor(router): Change router to get balance

--- backend/src/router/client.ts
+++ backend/src/router/client.ts
@@ -15,8 +15,8 @@ router.post('/deposito', validateToken, validateDepositOrDraft, clientController
 
 router.post('/saque', validateToken, validateDepositOrDraft, clientController.draft);
 
-router.get('/ativos', clientController.findAllClientAssets);
+router.get('/ativos', validateToken, clientController.findAllClientAssets);
 
-router.get('/:codCliente', validateToken, clientController.availableBalance);
+router.get('/saldo', validateToken, clientController.availableBalance);
 
 export default router;
