7aa36300805219fc24b973afca128cc13076c066
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-21 17:39:59 -0300

    refactor(controller.investment): Add validation for buy and sell

--- backend/src/controller/controller.investment.ts
+++ backend/src/controller/controller.investment.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import investmentService from '../service/service.investment';
+import HttpException from '../shared/http.exception';
 
 async function buyAsset(request: Request, response: Response) {
   const { body } = request;
+
+  const { client } = response.locals;
+
+  if (Number(body.CodCliente) !== Number(client.CodCliente)) {
+    throw new HttpException(
+      'Não é possível comprar ativo para outro cliente.',
+      StatusCodes.UNAUTHORIZED,
+    );
+  }
 
   const assetPurchased = await investmentService.buyOrSellAsset(body, 'buy');
 
@@ -12,6 +22,15 @@ async function buyAsset(request: Request, response: Response) {
 
 async function sellAsset(request: Request, response: Response) {
   const { body } = request;
+
+  const { client } = response.locals;
+
+  if (Number(body.CodCliente) !== Number(client.CodCliente)) {
+    throw new HttpException(
+      'Não é possível vender ativo de outro cliente.',
+      StatusCodes.UNAUTHORIZED,
+    );
+  }
 
   const assetSold = await investmentService.buyOrSellAsset(body, 'sell');
 
