b59bae4cca19ef31170278d21ea8093dc18277e2
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-21 11:29:23 -0300

    fix(prisma): Remove prisma from inside directory `src`

--- backend/src/prisma/migrations/20220718224836_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `username` to the `Cliente` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Cliente` ADD COLUMN `username` VARCHAR(191) NOT NULL;
--- backend/src/prisma/migrations/20220718225709_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `NomeAtivo` to the `Ativo` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Ativo` ADD COLUMN `NomeAtivo` VARCHAR(191) NOT NULL;
