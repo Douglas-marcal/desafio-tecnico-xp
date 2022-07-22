121e57c9277c725b1dc59299b1d7c6a01f72d4fd
Douglas Marcal <douglasymarcal@gmail.com>
2022-07-22 16:58:10 -0300

    fix(service.asset): Add types at service functions

--- backend/src/service/service.asset.ts
+++ backend/src/service/service.asset.ts
@@ -1,3 +1,4 @@
+import { Ativo } from '@prisma/client';
 import { StatusCodes } from 'http-status-codes';
 import { Asset, ResponseAsset } from '../interface';
 import AssetModel from '../model/model.asset';
@@ -5,8 +6,8 @@ import HttpException from '../shared/http.exception';
 
 const assetModel = new AssetModel();
 
-async function getAllAssets() {
-  const assets = await assetModel.getAllAssets();
+async function getAllAssets(): Promise<Array<ResponseAsset>> {
+  const assets: Array<Ativo> = await assetModel.getAllAssets();
 
   const assetsFormatted = assets.map((asset) => ({
     ...asset,
@@ -16,12 +17,12 @@ async function getAllAssets() {
   return assetsFormatted;
 }
 
-function getByAssetCode(assetCode: number) {
+function getByAssetCode(assetCode: number): Promise<Ativo | null> {
   return assetModel.getByAssetCode(assetCode);
 }
 
 async function registerAsset(asset: Asset): Promise<ResponseAsset> {
-  const alreadyExist = await assetModel.getByAssetName(asset.NomeAtivo);
+  const alreadyExist: Ativo | null = await assetModel.getByAssetName(asset.NomeAtivo);
 
   const message = 'Não foi possível criar, já existe um ativo com este nome.';
 
@@ -29,9 +30,9 @@ async function registerAsset(asset: Asset): Promise<ResponseAsset> {
     throw new HttpException(message, StatusCodes.CONFLICT);
   }
 
-  const registeredAsset = await assetModel.registerAsset(asset);
+  const registeredAsset: Ativo = await assetModel.registerAsset(asset);
 
-  const response = {
+  const response: ResponseAsset = {
     ...registeredAsset,
     Valor: Number(registeredAsset.Valor),
   };
